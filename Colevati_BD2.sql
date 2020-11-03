40846f6ab1b313a70fd38b45d360dc1e16c87275
albarosss <61046309+albarosss@users.noreply.github.com>
2020-11-03 11:18:20 -0300

    Update Colevati_BD2.sql

--- Colevati_BD2.sql
+++ Colevati_BD2.sql
@@ -3,19 +3,6 @@ GO
 USE projetos
 GO
 
-/*
-Criar as tabelas da forma que foram diagramadas, obedecendo as seguintes restrições:
-A coluna users_id da tabela associativa é FK da coluna id, tabela users
-A coluna projects_id da tabela associativa é FK da coluna id, tabela projects
-A coluna date da tabela projects deve verificar se a data é posterior que 01/09/2014.
-Caso contrário, o registro não deve ser inserido
-A PK de projects deve ser de auto incremento, iniciando em 10001, com incremento de
-1
-A PK de users deve ser de auto incremento, iniciando em 1, com incremento de 1
-O valor padrão da coluna password da tabela users, deverá ser 123mudar
-A coluna username da tabela users deve ter restrição de unicidade.
-*/
-
 CREATE TABLE projects(
 id					INT				NOT NULL		IDENTITY(10001,1),
 nome				VARCHAR(45)		NOT NULL,
@@ -45,12 +32,6 @@ FOREIGN KEY(projects_id) REFERENCES projects(id)
 )
 
 --TRUNCATE TABLE user_has_projects
-/*
-Modificar a coluna username da tabela Users para varchar(10)
-Modificar a coluna password da tabela Users para varchar(8)
-*/
-
---ARRUMAR QUE DEU ERRO
 
 --DROP TABLE user_has_projects
 --TRUNCATE TABLE USERS
@@ -95,24 +76,6 @@ VALUES
 (4,10002),
 (2,10002)
 
-/*
-- O projeto de Manutenção atrasou, mudar a data para 12/09/2014
-- O username de aparecido (usar o nome como condição de mudança) está feio, mudar para
-Rh_cido
-- Mudar o password do username Rh_maria (usar o username como condição de mudança)
-para 888@*, mas a condição deve verificar se o password dela ainda é 123mudar
-- O user de id 2 não participa mais do projeto 10002, removê-lo da associativa
-- Adicionar uma coluna budget DECIMAL(7,2) NULL na tabela Project
-- Atualizar a coluna budget com:
-5750.00 para id 10001
-7850.00 para id 10002
-9530.00 para id 10003
-- Consultar:
-1) username e password da Ana
-2) nome, budget e valor hipotético de um budget 25% maior
-3) id, nome e e-mail do usuário que ainda mantém o password padrão (123mudar)
-4) id, nome dos budgets cujo valor está entre 2000.00 e 8000.00
-*/
 
 --SELECT * FROM projects
 --SELECT * FROM users
